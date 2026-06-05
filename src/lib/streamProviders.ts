4e64e57ceb90fc904dea1b3562c43f132ee5ef0f
aninditoyoga <anindito@gmail.com>
2026-06-05 11:30:10 +0700

    update streamprovider

--- src/lib/streamProviders.ts
+++ src/lib/streamProviders.ts
@@ -4,37 +4,124 @@ type MediaIds = {
 };
 
 type StreamProvider = {
+  id: string;
   name: string;
+  baseUrl: string;
   buildMovieUrl: (ids: MediaIds) => string | null;
+};
+
+const withImdbFallback = (
+  ids: MediaIds,
+  build: (id: string) => string,
+  preferImdb = false
+) => {
+  const id = preferImdb ? ids.imdbId || ids.tmdbId : ids.tmdbId || ids.imdbId;
+
+  return id ? build(id) : null;
 };
 
 export const movieStreamProviders: StreamProvider[] = [
   {
-    name: "VidSrc Online",
-    buildMovieUrl: ({ imdbId }) =>
-      imdbId ? `https://vidsrc.online/embed/movie/${imdbId}` : null,
+    id: "02moviedownloader",
+    name: "02MovieDownloader",
+    baseUrl: "https://02moviedownloader.com",
+    buildMovieUrl: ({ tmdbId }) => `https://02moviedownloader.com/movie/${tmdbId}`,
   },
   {
-    name: "VidSrc FYI",
-    buildMovieUrl: ({ imdbId }) =>
-      imdbId ? `https://vidsrc.fyi/embed/movie/${imdbId}` : null,
+    id: "anyembed",
+    name: "AnyEmbed",
+    baseUrl: "https://player.anyembed.com",
+    buildMovieUrl: ({ tmdbId }) => `https://player.anyembed.com/movie/${tmdbId}`,
   },
   {
-    name: "VidSrc CC",
-    buildMovieUrl: ({ imdbId }) =>
-      imdbId ? `https://vidsrc.cc/v2/embed/movie/${imdbId}` : null,
+    id: "cinesu",
+    name: "CineSu",
+    baseUrl: "https://embed.su",
+    buildMovieUrl: ({ tmdbId }) => `https://embed.su/embed/movie/${tmdbId}`,
   },
   {
-    name: "VidSrc Wiki",
-    buildMovieUrl: ({ tmdbId }) => `https://vidsrc.wiki/embed/movie/${tmdbId}`,
+    id: "fmovies4u",
+    name: "FMovies4U",
+    baseUrl: "https://fmovies4u.com",
+    buildMovieUrl: ({ tmdbId }) => `https://fmovies4u.com/watch/movie/${tmdbId}`,
   },
   {
-    name: "VidSrc SBS",
-    buildMovieUrl: ({ tmdbId }) => `https://vidsrc.sbs/embed/movie/${tmdbId}`,
+    id: "fshare",
+    name: "FshareTV",
+    baseUrl: "https://fsharetv.co",
+    buildMovieUrl: ({ tmdbId }) => `https://fsharetv.co/movie/${tmdbId}`,
   },
   {
+    id: "icefy",
+    name: "Icefy",
+    baseUrl: "https://icefy.tv",
+    buildMovieUrl: ({ tmdbId }) => `https://icefy.tv/movie/${tmdbId}`,
+  },
+  {
+    id: "peachify",
+    name: "Peachify",
+    baseUrl: "https://peachify.net",
+    buildMovieUrl: ({ tmdbId }) => `https://peachify.net/embed/movie/${tmdbId}`,
+  },
+  {
+    id: "popr",
+    name: "Popr",
+    baseUrl: "https://popr.tv",
+    buildMovieUrl: ({ tmdbId }) => `https://popr.tv/movie/${tmdbId}`,
+  },
+  {
+    id: "streammafia",
+    name: "MafiaEmbed",
+    baseUrl: "https://streammafia.com",
+    buildMovieUrl: ({ tmdbId }) => `https://streammafia.com/embed/movie/${tmdbId}`,
+  },
+  {
+    id: "tulnex",
+    name: "Tulnex",
+    baseUrl: "https://tulnex.com",
+    buildMovieUrl: ({ tmdbId }) => `https://tulnex.com/embed/movie/${tmdbId}`,
+  },
+  {
+    id: "vidapi",
+    name: "VidApi",
+    baseUrl: "https://vidapi.ru",
+    buildMovieUrl: ({ tmdbId }) => `https://vidapi.ru/movie/${tmdbId}`,
+  },
+  {
+    id: "videasy",
+    name: "Videasy",
+    baseUrl: "https://player.videasy.net",
+    buildMovieUrl: ({ tmdbId }) => `https://player.videasy.net/movie/${tmdbId}`,
+  },
+  {
+    id: "vidnest",
+    name: "VidNest",
+    baseUrl: "https://vidnest.fun",
+    buildMovieUrl: ({ tmdbId }) => `https://vidnest.fun/movie/${tmdbId}`,
+  },
+  {
+    id: "vidrock",
+    name: "VidRock",
+    baseUrl: "https://vidrock.net",
+    buildMovieUrl: ({ tmdbId }) => `https://vidrock.net/movie/${tmdbId}`,
+  },
+  {
+    id: "vidsrc",
+    name: "VidSrc",
+    baseUrl: "https://vsembed.ru",
+    buildMovieUrl: ({ tmdbId }) => `https://vsembed.ru/embed/movie?tmdb=${tmdbId}`,
+  },
+  {
+    id: "vidzee",
+    name: "VidZee",
+    baseUrl: "https://vidzee.wtf",
+    buildMovieUrl: ({ tmdbId }) => `https://vidzee.wtf/movie/${tmdbId}`,
+  },
+  {
+    id: "vixsrc",
     name: "VixSrc",
-    buildMovieUrl: ({ tmdbId }) => `https://vixsrc.to/movie/${tmdbId}`,
+    baseUrl: "https://vixsrc.to",
+    buildMovieUrl: (ids) => withImdbFallback(ids, (id) => `https://vixsrc.to/movie/${id}`),
   },
 ];
 
